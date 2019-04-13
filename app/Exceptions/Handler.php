6a72dfd7e090cd839bd6ad9e892a4a7d24e6df85
nixonnht <mahsudunnabi79@gmail.com>
2019-04-13 16:54:13 +0600

    Handles the exceptins

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -48,6 +49,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // if($request->expectsJson()){
+        //     if($exception instanceof ModelNotFoundException){
+        //     return response()->json([
+        //         'errors' => 'Product Model not found'
+        //     ],Response::HTTP_NOT_FOUND);
+        //     }
+        //     if($exception instanceof ModelNotFoundException){
+        //     return response()->json([
+        //         'errors' => 'Incorrect route'
+        //     ],Response::HTTP_NOT_FOUND);
+        //     }
+        // } 
+        //dd($exception);
         return parent::render($request, $exception);
     }
 }
